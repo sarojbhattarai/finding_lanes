c70f78e97bf0c6ae32684c9a84fb0564c62b78f5
sarojbhattarai <sarojbhattarai800@gmail.com>
2019-02-07 11:39:09 +0545

    function

--- lanes.py
+++ lanes.py
@@ -6,9 +6,13 @@ image = cv2.imread('test_image.jpg')
 # the relative intensity of each pixel in the image
 
 lane_image = np.copy(image)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray, (5,5), 0)
-canny = cv2.Canny(blur, 50, 150)
+def canny(image):
+	gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+	blur = cv2.GaussianBlur(gray, (5,5), 0)
+	canny = cv2.Canny(blur, 50, 150)
+	return canny
+	
+canny = canny(lane_image)
  
 # to render the image we use imshow
 
